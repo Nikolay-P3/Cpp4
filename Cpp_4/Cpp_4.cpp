0d046661f96a3e337d9e3644740b755c2c494be3
Nicholas Pankratov <48381479+pankratov35@users.noreply.github.com>
2019-06-12 10:42:50 +0300

    Add files via upload

---
+++ Cpp_4/Cpp_4.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cout << "Input string ->";
+	getline(std::cin, str);
+	cout << "\n" << std::count_if(str.begin(), str.end(), ::ispunct) << std::endl;
+}
